55cb04b5272f4e123f2fa2c7426643236ab1910c
preciz <barna.kovacs.codes@gmail.com>
2019-10-09 10:05:28 +0200

    Remove not used Logger from extra_applications in mix.exs

--- mix.exs
+++ mix.exs
@@ -17,9 +17,7 @@ defmodule WordsToInteger.MixProject do
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   # Run "mix help deps" to learn about dependencies.
